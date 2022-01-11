cfa8fe3939ba368adb4cfcfbb40e6bb05a826053
Alex Krantz <alex@krantz.dev>
2022-01-10 18:35:24 -0800

    Add router with logging

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+use axum::{routing::get, Router, Server};
 use eyre::WrapErr;
 use std::env;
 use tracing::info;
+
+mod logging;
 
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
@@ -9,11 +12,21 @@ async fn main() -> eyre::Result<()> {
     tracing_subscriber::fmt::init();
 
     // Get the listen address
-    let address = env::var("ADDRESS").wrap_err("missing ADDRESS environment variable")?;
+    let address = env::var("ADDRESS")
+        .wrap_err("missing ADDRESS environment variable")?
+        .parse()
+        .wrap_err("invalid listen address format")?;
+
+    // Setup the routes
+    let router = Router::new()
+        .route("/", get(|| async { "hello world" }))
+        .layer(logging::layer());
 
     // Start the server
     info!(%address, "listening and ready to handle requests");
-    // TODO: actually start it
+    Server::bind(&address)
+        .serve(router.into_make_service())
+        .await?;
 
     Ok(())
 }
