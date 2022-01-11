d33689bbca7674b147d0cdcac909d998aa661841
Alex Krantz <alex@krantz.dev>
2022-01-10 18:20:56 -0800

    Add logging and error handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use eyre::WrapErr;
+use std::env;
+use tracing::info;
+
+#[tokio::main]
+async fn main() -> eyre::Result<()> {
+    color_eyre::install()?;
+    dotenv::dotenv().wrap_err("failed to find .env configuration file")?;
+    tracing_subscriber::fmt::init();
+
+    // Get the listen address
+    let address = env::var("ADDRESS").wrap_err("missing ADDRESS environment variable")?;
+
+    // Start the server
+    info!(%address, "listening and ready to handle requests");
+    // TODO: actually start it
+
+    Ok(())
 }
